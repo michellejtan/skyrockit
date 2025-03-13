55ce1192123b101dca33fdfb41c669c249a8d8e4
michellejtan <michelle0223@gmail.com>
2025-03-13 08:39:56 -0700

    wip: add routes for creating a new app & reading app index & manage incoming form data

--- controllers/applications.js
+++ controllers/applications.js
@@ -4,12 +4,44 @@ const router = express.Router();
 const User = require('../models/user.js');  //isn't this for application, so why require model? we have deal with parent, accessing submodel 
 
 // GET /users/:userId/applications
-router.get('/', (req, res) => { // the forward slash "/" indicating anything after "application" in path
+router.get('/', async (req, res) => { // the forward slash "/" indicating anything after "application" in path
     try {
-        res.render('applications/index.ejs');
-      } catch (error) {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Render index.ejs, passing in all of the current user's
+        // applications as data in the context object.
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        });
+    } catch (error) {
+        // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/');
-      }  });
+    }
+});
+
+// GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+//POST /users/:userID/applications
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // applications array of the current user
+      currentUser.applications.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
   
 module.exports = router;
